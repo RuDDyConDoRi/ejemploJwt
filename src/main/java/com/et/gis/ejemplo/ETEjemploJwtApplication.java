5fc148f5a330cba5b2f47295b2bfbacbd50bae03
Ruddy Condori <ruddy.condori@et.bo>
2019-04-03 10:44:53 -0400

    creando proyecto de ejemplo JWT

---
+++ src/main/java/com/et/gis/ejemplo/ETEjemploJwtApplication.java
@@ -0,0 +1,12 @@
+package com.et.gis.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETEjemploJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETEjemploJwtApplication.class, args);
+	}
+}
---
+++ src/main/java/com/et/gis/ejemplo/security/model/AuthorityName.java
@@ -0,0 +1,5 @@
+package com.et.gis.ejemplo.security.model;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
